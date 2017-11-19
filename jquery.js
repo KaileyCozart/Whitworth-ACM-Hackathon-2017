7c405b8358e71538cc6d27c242592db0738c8947
KaileyCozart <kaileync@comcast.net>
2017-11-18 19:00:52 -0800

    Improved Print Feature and Icon

--- jquery.js
+++ jquery.js
@@ -12,52 +12,79 @@ $(function(){
     });
 });
 
-// Add New Items To List And Fade Out Added Li Tags On Click
-
-$(function() {
-
-var $newItemButton = $('#newItemButton');
-var $newItemForm = $('#newItemForm');
-var $textInput = $('input:text');
-
-$newItemButton.show();
-$newItemForm.hide();
-
-$('#showForm').on('click', function(){
-    $newItemButton.hide();
-    $newItemForm.show();
-});
-
-$newItemForm.on('submit', function(event){
-    event.preventDefault();
-    var newText = $textInput.val();
-    $('li:last').after('<li>' + newText + '</li>');
-    $newItemButton.show();
-    $newItemForm.hide();
-    $textInput.val('');
-
-    $(function(){
-        $('li').on('click', function(){
-            $(this).hide();
-            // $(this).fadeOut(800);
-        });
-    });
-
-});
-
-});
-
 // Fade Out Original Li Tags On Click
 
 $(function(){
     $('li').on('click', function(){
-        $(this).hide();
+        $(this).fadeOut(0);
     });
     $('li').addClass('defaultItem');
 });
 
-// Print Page Function
+// Add New Items To List And Fade Out Added Li Tags On Click
 
-function pintPage() {
-    window.print();
-}
+$(function() {
+    
+    var $newItemButton = $('#newItemButton');
+    var $newItemForm = $('#newItemForm');
+    var $textInput = $('input:text');
+    
+    $newItemButton.show();
+    $newItemForm.hide();
+    
+    $('#showForm').on('click', function(){
+        $newItemButton.hide();
+        $newItemForm.show();
+    });
+    
+    $newItemForm.on('submit', function(event){
+        event.preventDefault();
+        var newText = $textInput.val();
+        $('li:last').after('<li>' + '<em>' + newText + '</em>' + '</li>');
+        $newItemButton.show();
+        $newItemForm.hide();
+        $textInput.val('');
+        
+        $(function(){
+            $('li').on('click', function(){
+                // $(this).hide();
+                $(this).fadeOut(0);
+            });
+        });
+        
+    });
+    
+});
+
+
+// Print Page Function
+$(function () {
+    $(".printPage").click(function () {
+        var contents = $("ul").html();
+        var frame1 = $('<iframe />');
+        frame1[0].name = "frame1";
+        frame1.css({ "position": "absolute", "top": "-1000000px" });
+        $("body").append(frame1);
+        //Create a HTML document
+        var frameDoc = frame1[0].contentWindow ? frame1[0].contentWindow : frame1[0].contentDocument.document ? frame1[0].contentDocument.document : frame1[0].contentDocument;
+        frameDoc.document.open();
+        frameDoc.document.write('<html><head><title>BLACK FRIDAY SHOPPING LIST</title>');
+        frameDoc.document.write('</head><body>');
+        frameDoc.document.write('<h1 class="printH1">My Black Friday Shopping List:</h1>');
+        //Link to a sepatate CSS file
+        frameDoc.document.write('<link href="printStyles.css" rel="stylesheet" type="text/css" />');
+        //Add the list items
+        frameDoc.document.write(contents);
+        frameDoc.document.write('</body></html>');
+        frameDoc.document.close();
+        setTimeout(function () {
+            window.frames["frame1"].focus();
+            window.frames["frame1"].print();
+            frame1.remove();
+        }, 500);
+    });
+});
+
+$(function () {
+    $()
+});
